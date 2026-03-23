68a8b7d85acaec3072c03e97d327c92844f4180c
Areeb455 <areebimam466@gmail.com>
2026-03-23 20:25:20 +0530

    connected frontend to deployed backend

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ export const predictFood = async (file) => {
   const formData = new FormData();
   formData.append("file", file);
 
-  const response = await fetch("http://127.0.0.1:8000/predict-food", {
+  const response = await fetch("https://medule-main.onrender.com", {
     method: "POST",
     body: formData,
   });
